b254aabfdb0a2ac1aad7a3d9056c4a9a038f815a
unknown <greatzaochen@outlook.com>
2025-02-02 18:47:03 +0800

    [fix] 修复版本号问题

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,7 +13,7 @@
 #include <QJsonObject>
 #include <QTimer>
 
-QString local_version = "v2.4.1";
+QString local_version = "v2.4.3";
 
 MainWindow::MainWindow(QWidget *parent)
     : ElaWidget(parent)
